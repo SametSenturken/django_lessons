0ed28804c669e23b1af932d75956fc42d35498d3
SametSenturken <samet.senturken@gmail.com>
2025-12-09 21:11:58 +0000

    2. gün kaydedildi hareketli path

--- examples/myapp/urls.py
+++ examples/myapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", views.index, name = "index"),
+    path("details", views.details, name = "details"),
+    path("list", views.list, name = "list"),
+    path("<category>", views.getProductsByCategory)
 ]
--- examples/myapp/views.py
+++ examples/myapp/views.py
@@ -8,3 +8,21 @@ from django.http.response import HttpResponse
 def index(request):
     return HttpResponse("Merhaba")
 
+def details(request):
+    return HttpResponse("details")
+
+def list(request):
+    return HttpResponse("list")
+
+def getProductsByCategory(request, category):
+    category_text = None
+    if category == "bilgisayar":
+        category_text = "Lenovo, HP, Apple"
+    elif category == "telefon":
+        category_text = "samsung, xaomi, huawei"
+    else:
+        category_text = "Yanlış kategori seçimi"
+    
+    return HttpResponse(category_text)
+
+
